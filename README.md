f9ec43e828a60f53f6b58dc273dcc8898fef505f
ErnadoO <ernadoo@gmail.com>
2016-11-25 12:16:04 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+HTTPS Redirection
+===================
+
+HTTPS Redirection is an Extension for phpBB 3.1
+
+## Installation
+
+Clone into phpBB/ext/ernadoo/ext-https-redirection:
+
+    git clone https://github.com/ErnadoO/ext-https-redirection.git phpBB/ext/ernadoo/httpsredirection
+
+Go to "ACP" > "Customise" > "Extensions" and enable the "HTTPS Redirection" extension.
+
+## License
+
+[GPLv2](LICENSE)
---
+++ event/listener.php
@@ -0,0 +1,56 @@
+<?php
+/**
+*
+* HTTPS Redirection extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2014 ErnadoO <http://www.phpbb-services.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace ernadoo\httpsredirection\event;
+
+/**
+* Event listener
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class listener implements EventSubscriberInterface
+{
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\request\request	$request	Request object
+	* @access public
+	*/
+	public function __construct(\phpbb\request\request $request)
+	{
+		$this->request = $request;
+	}
+
+	/**
+	* Assign functions defined in this class to event listeners in the core
+	*
+	* @return	array
+	* @static
+	* @access	public
+	*/
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.common'	=> 'set_header',
+		);
+	}
+
+	public function set_header()
+	{
+		if (!$this->request->is_secure() && PHP_SAPI != 'cli')
+		{
+			header('Location: https://' . $this->request->server('HTTP_HOST') . $this->request->server('REQUEST_URI'), true, 301);
+			exit;
+		}
+	}
+}
